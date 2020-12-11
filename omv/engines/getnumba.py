06f56789fceb0aff4f9fa7f7dc5075d84f7d9e2e
Russell Jarvis <rjjarvis@asu.edu>
2020-12-11 11:21:47 +1100

    methods for making numba model OSB validation compliant

---
+++ omv/engines/getnumba.py
@@ -0,0 +1,30 @@
+import os
+from omv.common.inout import inform, check_output
+from omv.engines.utils.wdir import working_dir
+
+def install_numba():
+    try:
+        print(check_output(['pip', 'install', 'numba']))  # This should ideally be automatically installed with PyNN...
+        print(check_output(['pip', 'install', 'quantities']))  # This should ideally be automatically installed with PyNN...
+        print(check_output(['pip', 'install', 'neo==0.5.1']))  # This should ideally be automatically installed with PyNN...
+
+        install_root = os.environ['HOME']
+
+        pyNN_src = 'PyNN_src'
+        with working_dir(install_root):
+            check_output(['git', 'clone', 'https://github.com/NeuralEnsemble/PyNN.git', pyNN_src])
+
+        path = os.path.join(install_root, pyNN_src)
+
+        with working_dir(path):
+            print(check_output(['git','checkout','neuroml']))  # neuroml branch has the latest NML2 import/export code!
+            #check_output(['git','checkout','master'])
+            print(check_output(['python', 'setup.py', 'install']))
+            print(check_output(['pwd']))
+            print("Finished attempting to install PyNN")
+        #import pyNN
+        m = 'Successfully installed pyNN...'
+    except Exception as e:
+        m = 'ERROR during install_pynn: %s'%e
+    finally:
+        inform(m)
---
+++ omv/engines/numba_izhi.py
@@ -0,0 +1,55 @@
+import os
+import subprocess as sp
+
+from omv.common.inout import inform, trim_path, check_output, is_verbose
+from omv.engines.engine import OMVEngine, EngineExecutionError
+
+
+class NumbaEngine(OMVEngine):
+
+    name = "Numba"
+
+    @staticmethod
+    def is_installed(version):
+        ret = True
+        try:
+
+            ret_str = sp.check_output(['python -c "import numba; print(numba.__version__)"'], shell=True,stderr=sp.STDOUT)
+            ret = len(ret_str) > 0
+
+            if isinstance(ret_str, bytes):
+                ret_str = ret_str.decode('utf-8')
+
+            ret = 'v%s'%ret_str.strip()
+            if is_verbose():
+                inform("numba %s is correctly installed..." % ret, indent=2, verbosity=2)
+
+        except Exception as err:
+            inform("Couldn't import numba into Python: ", err, indent=1, verbosity=2)
+            ret = False
+        return ret
+
+    @staticmethod
+    def install(version):
+        from omv.engines.getnumba import install_numba
+        #install_pynn
+        home = os.environ['HOME']
+        inform('Will fetch and install the latest numba', indent=2)
+        install_numbal()
+        inform('Done, numba is correctly installed...', indent=2)
+
+
+    def run(self):
+        try:
+            inform("Running file %s with %s" % (trim_path(self.modelpath), self.name), indent=1)
+            self.stdout = check_output(['python', self.modelpath],
+                                          cwd=os.path.dirname(self.modelpath))
+            self.returncode = 0
+        except sp.CalledProcessError as err:
+            self.returncode = err.returncode
+            self.stdout = err.output
+            raise EngineExecutionError
+        except Exception as err:
+            inform("Another error with running %s: "%self.name, err, indent=1)
+            self.returncode = -1
+            self.stdout = "???"
